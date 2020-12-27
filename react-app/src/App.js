018e3246ef3f6136359ecafc27d58e6ab152a2ed
yousejp <yousejp25@gmail.com>
2020-12-27 17:40:40 +0900

    stateの勉強

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,38 @@
-// import React, { Component } from 'react';
-import React from 'react';
+import React, { Component } from 'react';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// User.propTypes = {
+//   name: PropTypes.string,
+//   age: PropTypes.number,
+// };
+
+const App = () => <Counter></Counter>;
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+  // stateを変更するときは{ count: 0 }ではなくsetStateを使う
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handleMinusButton}>-1</button>
+        <button onClick={this.handlePlusButton}>+1</button>
+      </React.Fragment>
+    );
+  }
+}
+
+// export { CounterApp };
 
 const User = (props) => {
   return (
@@ -8,26 +41,28 @@ const User = (props) => {
     </h2>
   );
 };
+
 User.defaultProps = {
   age: 78,
 };
 
-const App = () => {
-  const profiles = [
-    { name: 'pekora', age: 111 },
-    { name: 'marin', age: 17 },
-    { name: 'rushia' },
-  ];
-  return (
-    <React.Fragment>
-      <div>
-        {profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />;
-        })}
-      </div>
-    </React.Fragment>
-  );
-};
+// const App2 = () => {
+//   const profiles = [
+//     { name: 'pekora', age: 111 },
+//     { name: 'marin', age: 17 },
+//     { name: 'rushia' },
+//     // { name: 78 }, nameがnumber型だとエラーになる
+//   ];
+//   return (
+//     <React.Fragment>
+//       <div>
+//         {profiles.map((profile, index) => {
+//           return <User name={profile.name} age={profile.age} key={index} />;
+//         })}
+//       </div>
+//     </React.Fragment>
+//   );
+// };
 
 // class App extends Component {
 //   render() {
@@ -47,5 +82,7 @@ const App = () => {
 //     return text;
 //   }
 // }
+
+// export { App };
 
 export default App;
