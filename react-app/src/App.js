b2ba5e3a4135537625b3c85dfa292e978933f335
yousejp <yousejp25@gmail.com>
2020-12-27 16:52:14 +0900

    propsの勉強

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,51 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <h1 className="App-title">
-        Welcome to Reactぺこだにぇ
-        <br />
-        yarn run start でローカルサーバーが立ち上がる
-      </h1>
-    );
-  }
-}
+const User = (props) => {
+  return (
+    <h2>
+      HA↑HA↑HA↑HA↑ I am {props.name}! {props.age} years old
+    </h2>
+  );
+};
+User.defaultProps = {
+  age: 78,
+};
+
+const App = () => {
+  const profiles = [
+    { name: 'pekora', age: 111 },
+    { name: 'marin', age: 17 },
+    { name: 'rushia' },
+  ];
+  return (
+    <React.Fragment>
+      <div>
+        {profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />;
+        })}
+      </div>
+    </React.Fragment>
+  );
+};
+
+// class App extends Component {
+//   render() {
+//     const welcome = 'Welcome to Reactぺこだにぇなのです';
+//     const text = (
+//       <React.Fragment>
+//         <h1 className="head">
+//           {welcome}
+//           <br />
+//           yarn start でローカルサーバーが立ち上がる
+//         </h1>
+//         <p>Reactの制約で一つのタグしか返してはいけない</p>
+//         <User name={'usada'} age={111} />
+//         <User name={'marin'} age={17} />
+//       </React.Fragment>
+//     );
+//     return text;
+//   }
+// }
 
 export default App;
